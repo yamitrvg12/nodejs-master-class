b8e9df25ef14d811774035d20555076a676d8e6a
yamitrvg12 <yamitrvg12@gmail.com>
2019-02-03 20:16:50 -0500

    Handling HTTP GET Requests

--- RESTful API Using Express/index.js
+++ RESTful API Using Express/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const courses = [
+    { id: 1, name: 'course 1'},
+    { id: 2, name: 'course 2'},
+    { id: 3, name: 'course 3'}
+];
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
@@ -11,8 +17,13 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    // res.send(req.params);
-    // res.send(req.query);
+    const course = courses.filter(cr => cr.id === parseInt(req.params.id));
+
+    if (course.length > 0) {
+        res.send(course);
+    } else {
+        res.status(404).send('Not found');
+    }
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
