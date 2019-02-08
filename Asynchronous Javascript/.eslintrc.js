90a2cef8c73b98ccaf33827f2b6fb88dd3a4941e
yamitrvg12 <yamitrvg12@gmail.com>
2019-02-07 21:42:20 -0500

    Initial work!

---
+++ Asynchronous Javascript/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	"env": {
+		"es6": true,
+		"node": true
+	},
+	"extends": "airbnb",
+	"parserOptions": {
+		"ecmaVersion": 2015
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"no-tabs": 0,
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
