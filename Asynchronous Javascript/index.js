4acd19926435307a61c237246995c0d66541299e
yamitrvg12 <yamitrvg12@gmail.com>
2019-02-07 22:04:20 -0500

    Example of callback function

--- Asynchronous Javascript/index.js
+++ Asynchronous Javascript/index.js
@@ -1,16 +1,27 @@
-function getUser(id) {
+function getUser(id, callback) {
 	setTimeout(() => {
 		console.log('Reading a user from a database...');
-		return {
-			id,
-			gitHubUsername: 'yamitrvg12',
-		};
+		callback({ id, gitHubUsername: 'yamitrvg12' });
+	}, 2000);
+}
+
+function getRepositories(userName, callback) {
+	setTimeout(() => {
+		console.log(`Calling github API from: ${userName}...`);
+		callback(['repo 1', 'repo 2', 'repo 3', 'repo 4']);
 	}, 2000);
 }
 
 console.log('before');
 
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+	// Get the userName
+	console.log(`User: ${user.gitHubUsername}`);
+
+	// Get the respositories
+	getRepositories(user.gitHubUsername, (repos) => {
+		console.log(`Repositories: ${repos}`);
+	});
+});
 
 console.log('after');
