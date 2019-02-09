4012bd35fd135f58976d5172e9c79dec2536c682
yamitrvg12 <yamitrvg12@gmail.com>
2019-02-09 09:53:23 -0500

    Schemas

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -2,6 +2,17 @@ const debug = require('debug')('app:mongodb');
 const mongoose = require('mongoose');
 
 // Connection string, name of the database
-mongoose.connect('mongodb://localhost:27017/playground', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
 	.then(() => debug('Connected To MongoDB...'))
 	.catch(err => debug(`Could not connect to MongoDB: ${err.message}`));
+
+const courseSchema = new mongoose.Schema({
+	name: String,
+	author: String,
+	tags: [String],
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+	isPublished: Boolean,
+});
