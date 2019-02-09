f179cdaef897f551c120674ddefe4ac3e610fe45
yamitrvg12 <yamitrvg12@gmail.com>
2019-02-08 23:37:46 -0500

    initial work! MongoDB

---
+++ mongo-demo/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	"env": {
+		"es6": true,
+		"node": true
+	},
+	"extends": "airbnb",
+	"parserOptions": {
+		"ecmaVersion": 8
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"no-tabs": 0,
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
